74893cedbe09606149e3cd327cb0176ac95e09e8
DevElkami <m.reignier@elkami.fr>
2022-12-15 17:34:16 +0100

    Create .NET MAUI

---
+++ MauiPasswordVault/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiPasswordVault
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiPasswordVault/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiPasswordVault
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
