8fbb52792e8bf97f8ea51fadff201ea53f805c01
DevElkami <m.reignier@elkami.fr>
2023-01-20 08:06:58 +0100

    Add log

--- WinformPasswordVault/Program.cs
+++ WinformPasswordVault/Program.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using NLog;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+using System;
+using System.Configuration;
+using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -13,20 +20,48 @@ namespace WinformPasswordVault
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                String directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "DevElkami", nameof(VaultCore));
+                FileTarget logfile = new("logfile")
+                {
+                    FileName = Path.Combine(directory, "log.txt"),
+                    CreateDirs = true
+                };
 
-            // Quick translate
+                logfile.Layout = Layout.FromString("${longdate}|${event-properties:item=EventId_Id}|${uppercase:${level}}|${logger}|${message} ${exception:format=tostring}");
+                LoggingConfiguration config = new();
+                config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
+            }
+            catch (Exception except)
+            {
+                Debug.WriteLine(except.ToString());
+                // No log, don't care
+            }
+
+            LogManager.GetLogger(nameof(WinformPasswordVault)).Info("Started");
+
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                // Quick translate
 #if DEBUG
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
 #else
             if (Thread.CurrentThread.CurrentUICulture.IetfLanguageTag != "fr-FR")
                 Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
 #endif
 
-            // Run app
-            Application.Run(new MainForm());
+                // Run app
+                Application.Run(new MainForm());
+            }
+            catch (Exception except)
+            {
+                LogManager.GetLogger(nameof(WinformPasswordVault)).Fatal(except.ToString());
+            }
         }
     }
 }
