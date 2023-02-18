9a9225ca8711e7234979ea1f7f150e61140cc852
DevElkami <m.reignier@elkami.fr>
2023-02-18 08:13:34 +0100

    Add view model

---
+++ MauiPasswordVault/ViewModel/InitViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiPasswordVault.ViewModel
+{
+    public class InitViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
